ce87075fd0c975770ccd50d3960f5131ea89f333
Helder <hmedeiros211@gmail.com>
2025-01-18 12:09:56 +0100

    feat(auth): add CustomRefreshToken for JWT authentication

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,15 +1,10 @@
 #https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#usage
-
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
-
 from django.urls import path
 
-from .views import get_notes, CustomTokenObtainPairView
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshToken
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CustomRefreshToken.as_view(), name='token_refresh'),
     path('notes/', get_notes)
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -49,6 +49,37 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         except:
             return Response({'success': False})
 
+class CustomRefreshToken(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            
+            request.data['refresh'] = refresh_token
+            
+            response = super().post(request, *args, **kwargs)
+            
+            tokens = response.data
+            access_token = tokens['access']
+
+            res = Response()
+
+            res.data = {'refreshed': True}
+            
+            res.set_cookie(
+                key="access_token",
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+
+            return res
+
+        except:
+            return Response({'refreshed': True})
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_notes(request):

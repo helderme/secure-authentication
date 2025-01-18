354fd26e3e517bbff78f6960e33e4484f0e82b27
Helder <hmedeiros211@gmail.com>
2025-01-18 12:25:22 +0100

    feat(auth): add logout endpoint and functions

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,10 +1,11 @@
 #https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#usage
 from django.urls import path
 
-from .views import get_notes, CustomTokenObtainPairView, CustomRefreshToken
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshToken, logout
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomRefreshToken.as_view(), name='token_refresh'),
-    path('notes/', get_notes)
+    path('notes/', get_notes),
+    path('logout/', logout)
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -79,6 +79,16 @@ class CustomRefreshToken(TokenRefreshView):
         except:
             return Response({'refreshed': True})
 
+@api_view(['POST'])
+def logout(request):
+    try:
+        res = Response()
+        res.data = {'success': True}
+        res.delete_cookie('access_token', path='/', samesite='None')
+        res.delete_cookie('refresh_token', path='/', samesite='None')
+        return res
+    except:
+        return Response({'success': False})
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])

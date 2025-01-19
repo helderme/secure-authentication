83a6c1e080471a20638452f0ab9e134dabf6bec8
Helder <hmedeiros211@gmail.com>
2025-01-19 19:24:26 +0100

    .env

--- frontend/src/enpoints/api.js
+++ frontend/src/enpoints/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:8000/api/'
+const BASE_URL = process.env.API_BASE_URL;
 const LOGIN_URL = `${BASE_URL}token/`
 const REFRESH_URL = `${BASE_URL}token/refresh/`
 const NOTES_URL = `${BASE_URL}notes/`
